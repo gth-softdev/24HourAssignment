ddcbcf038ca03a9c27152aee73e86abd6809d0d6
gth-softdev <68547258+gth-softdev@users.noreply.github.com>
2020-09-01 09:32:08 -0400

    errors fixed

--- 24Hour.Data/User.cs
+++ 24Hour.Data/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace _24Hour.Data
 {
     public class User
     {
+        [Key]
+        public Guid id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
     }
 }
--- 24Hour.Models/Comment.cs
+++ 24Hour.Models/Comment.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _24Hour.Data;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,21 @@ namespace _24Hour.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public User Author
+        {
+            get
+            {
+
+            }
+        }
+        public Post CommentPost
+        {
+            get
+            {
+
+            }
+        }
     }
 }
