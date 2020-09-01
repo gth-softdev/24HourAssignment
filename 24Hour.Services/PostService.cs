0e8679ac7645b175253027510ae0460e5b91ab0a
Dgraber27 <drewg2727@gmail.com>
2020-09-01 11:15:20 -0400

    Changes to make Postservice funciton

---
+++ 24Hour.Services/PostService.cs
@@ -0,0 +1,79 @@
+﻿using _24Hour.Data;
+using _24Hour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Services
+{
+    public class PostService
+    {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    Author = _userId,
+                    Title = model.Title,
+                    Text = model.Text,
+
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Posts.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Notes
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new NoteListItem
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+        public Post GetPostById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Posts
+                        .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                    new Post
+                    {
+                        PostId = entity.PostId,
+                        Title = entity.Title,
+                        Text = entity.Text,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc
+                    };
+            }
+        }
+    }
+}
